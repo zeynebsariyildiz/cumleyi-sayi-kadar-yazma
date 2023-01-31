1aa85927fedbd86460b6cb37f13e3c36e9dcd794
Zeyneb Mehlika Sarıyıldız <sariyildiz.mehlika07@gmail.com>
2023-01-31 11:53:49 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cumleyi_sayi_kadar_yazma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen Bir Cümle Giriniz ");
+            string cumle = Console.ReadLine();
+            Console.WriteLine("Lütfen Bir Sayı Giriniz");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < sayi; i++)
+            {
+                Console.WriteLine("{0}. {1}", (i + 1), cumle);
+            }
+            Console.ReadKey();
+        }
+    }
+}
